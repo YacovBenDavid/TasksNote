e84183254f4a70b15275796077a6b1231dc878a2
YacovBenDavid <86020229+YacovBenDavid@users.noreply.github.com>
2021-06-16 22:08:12 +0300

    Add Tasksnote web site

---
+++ js/script.js
@@ -0,0 +1,121 @@
+﻿/** Array of TaskNote Class */
+var taskNotes = [];
+
+/** JSON Object from Local Stoarage  */
+var taskNotesLS = [];
+
+//Form Elements
+var formTitle = document.getElementById("taskTitle");
+var formDesc = document.getElementById("taskDesc");
+var formDate = document.getElementById("taskDate");
+var formTime = document.getElementById("taskTime");
+var formNoteColor = document.getElementsByName("noteColor");
+
+
+// Preview Elemnts
+let previewTitle = document.getElementById('titlePreview');
+let previewDesc = document.getElementById('textPreview');
+let previewDateTime = document.getElementById('dateTimePreview');
+
+let previewNote = document.querySelector('#notePreview');
+let previewNoteColorClass = 'note--thistle';
+
+// section of all tasks
+let sectionTasks = document.querySelector(".section-tasks");
+let taskContainer = document.querySelector("#all-Tasks-section");
+
+function updatePreviewNoteColor(object) {
+    var classNoteColor = object.id;
+    previewNote.classList.remove(previewNoteColorClass);
+    previewNoteColorClass = classNoteColor;
+    previewNote.classList.add(previewNoteColorClass);
+}
+
+
+
+/** Update Preview Task Note whet form canged */
+function updatePreview() {
+    previewTitle.innerText = formTitle.value;
+    previewDesc.innerText = formDesc.value;
+    previewDateTime.innerText = `${formDate.value} ${formTime.value}`;
+}
+
+
+function createNewTaskNote() {
+
+    if (validateRequiredFields()) {
+        let titleTask = previewNote.querySelector(".titleTaskNote").textContent;
+        let taskDeskText = previewNote.querySelector('.textTaskNote').textContent;
+        let noteClases = [];
+        let animate = true;
+        previewNote.classList.forEach(c => noteClases.push(c));
+        let taskDateTime = previewNote.querySelector(".dateTimeTask").textContent;
+
+        let taskNote = new TaskNote(titleTask, taskDeskText, noteClases, taskDateTime);
+
+
+        sectionTasks.setAttribute('display', 'block');
+
+        taskNote.CreateElement(taskContainer, animate);
+        TaskNote.updateLocalStorage();
+
+        resetForm();
+    } else {
+        alert("Please enter the required fields! (Title, Text, Date)");
+    }
+
+
+
+
+}
+function validateRequiredFields() {
+    const isOk = (field) => !(field === "");
+    const fields = [formTitle.value, formDesc.value, formDate.value];
+    return fields.every(isOk);
+}
+
+function resetForm() {
+    document.getElementById("taskNotForm").reset();
+    previewTitle.innerText = "";
+    previewDesc.innerText = "";
+    previewDateTime.innerText = "";
+
+
+}
+
+function loadPage() {
+
+    //get notes from local storage:
+    let lsData = localStorage.getItem("taskNotes");
+    if (lsData) {
+        taskNotesLS = JSON.parse(localStorage.getItem("taskNotes"));
+
+        if (taskNotesLS) {
+            // Convert from JSON Object to TaskNote Class Array
+
+            taskNotes = []
+
+            taskNotes = taskNotesLS.map(tn => TaskNote.parseObject(tn));
+
+            taskNotes.forEach(t => t.CreateElement(taskContainer, false));
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    /*console.log(taskNotes);*/
+
+
+
+}
+
+loadPage();
+
---
+++ js/taskNote.js
@@ -0,0 +1,104 @@
+﻿
+/**Task Note */
+class TaskNote {
+    constructor(title, desc, clases, taskDateTime) {
+        this.ID = TaskNote.generateID();
+        this.taskTitle = title;
+        this.taskDesk = desc;
+        this.taskDateTime = taskDateTime;
+        this.noteClases = clases;
+    }
+
+    CreateElement(taskSection, animate=true) {
+        var noteElement = document.createElement('taskNote');
+        var noteElement = document.createElement('div');
+        noteElement.classList.add("col");
+
+        noteElement.innerHTML = `
+
+            <taskNote id="note-${this.ID}">
+                <!-- Note Header -->
+                <div class="note__header">
+                    <div class="note__header-title titleTaskNote">${this.taskTitle}</div>
+                    <div class="note__header-delete">
+                        <i class="deleteNoteBtn icon-basic-elaboration-document-remove"></i>
+                    </div>
+                </div>
+
+                <!-- Note Main -->
+                <div class="note__main">
+                    <p class="note__main-text textTaskNote" readonly>${this.taskDesk}</p>
+                </div>
+
+                <!-- Note Footer -->
+                <div class="note__footer">
+                    <p class="dateTimeTask">${this.taskDateTime}</p>
+                </div>
+            </taskNote>`
+
+        const taskNoteNode = noteElement.querySelector(`#note-${this.ID}`);
+        this.noteClases.forEach(c => taskNoteNode.classList.add(c));
+        if (!animate) {
+            taskNoteNode.classList.remove("note-animate");
+
+        }
+
+        const deleteBtn = noteElement.querySelector('.deleteNoteBtn');
+        /*console.log(deleteBtn);*/
+
+        deleteBtn.addEventListener('click', () => {
+            noteElement.remove()
+            TaskNote.updateLocalStorage()
+        })
+
+
+        taskSection.appendChild(noteElement)
+
+    }
+
+    static updateLocalStorage() {
+        var taskSections = document.querySelector("#all-Tasks-section");
+
+        var taskNoteObjects = [];
+        var taskNotesElements = taskSections.querySelectorAll('taskNote');
+
+
+        taskNotesElements.forEach(function (taskNoteElement) {
+
+            let title = taskNoteElement.querySelector(".titleTaskNote").textContent;
+            let text = taskNoteElement.querySelector(".textTaskNote").textContent;
+            let taskClasses = [];
+            taskNoteElement.classList.forEach(c => taskClasses.push(c));
+            let taskDateTime = taskNoteElement.querySelector(".dateTimeTask").textContent;
+
+            var taskNote = new TaskNote(title, text, taskClasses, taskDateTime);
+
+            taskNoteObjects.push(taskNote);
+            //taskNote.CreateElement(taskSections);
+
+        })
+        localStorage.setItem('taskNotes', JSON.stringify(taskNoteObjects))
+    }
+
+    /**
+    Get TaskNote Class by JSON Object 
+     * @param {JSON} taskNoteObject
+     * @returns{TaskNote}
+     */
+    static parseObject(taskNoteObject) {
+
+        return Object.assign(new TaskNote, taskNoteObject);
+    }
+
+    static generateID() {
+        let lastID = 0;
+        let lsLastID = localStorage.getItem("taskNoteID");
+        if (lsLastID) {
+
+            lastID = lsLastID + 1;
+            
+        }
+        localStorage.setItem("taskNoteID", lastID);
+        return lastID;
+    }
+}
